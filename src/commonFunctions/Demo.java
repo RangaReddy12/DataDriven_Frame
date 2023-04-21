14756f48f8bc7ef6937be88a8af1a6018143ba48
MAX HUB <MAX HUB@DESKTOP-4M14A0P>
2023-04-21 11:46:57 +0530

    Modified values

--- src/commonFunctions/Demo.java
+++ src/commonFunctions/Demo.java
@@ -4,19 +4,19 @@ package commonFunctions;
 public class Demo {
 public void add()
 {
-	int a=456,b=98,c;
+	int a=5554345,b=67876,c;
 	c=a+b;
 	System.out.println(c);
 }
 public void div()
 {
-	int a=456,b=98,c;
+	int a=56756,b=78567,c;
 	c=a/b;
 	System.out.println(c);
 }
 public void mul()
 {
-	int a=456,b=98,c;
+	int a=45646,b=65465498,c;
 	c=a*b;
 	System.out.println(c);
 }
