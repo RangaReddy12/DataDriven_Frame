76c2cde0e2cd2617cc8fc877b226d9d13f31fe26
MAX HUB <MAX HUB@DESKTOP-4M14A0P>
2023-04-21 11:45:00 +0530

    new class commit

---
+++ src/commonFunctions/Demo.java
@@ -0,0 +1,24 @@
+
+package commonFunctions;
+
+public class Demo {
+public void add()
+{
+	int a=456,b=98,c;
+	c=a+b;
+	System.out.println(c);
+}
+public void div()
+{
+	int a=456,b=98,c;
+	c=a/b;
+	System.out.println(c);
+}
+public void mul()
+{
+	int a=456,b=98,c;
+	c=a*b;
+	System.out.println(c);
+}
+
+}
